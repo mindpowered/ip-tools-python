151669ed44a8033b381e28ef4d3cf94699a19633
Build <support@mindpowered.dev>
2020-12-10 06:24:04 +0000

    Version 0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='mindpowered-iptools',
+    version='0.0.1',
+    author="Mind Powered Corporation",
+    author_email="support@mindpowered.dev",
+    license="CPAL-1.0",
+    url="https://mindpowered.dev/",
+    description="IPTools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['iptools'],
+    packages=['mindpowered_iptools'],
+    package_dir={'mindpowered_iptools': 'wrappers'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'mindpowered-maglev',
+    ],
+)
